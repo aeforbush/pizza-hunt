15f390f43ec6a5c540973ee604b3625124d6eceb
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-11-16 13:11:47 -0700

    added crud for comments

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -7,6 +7,14 @@ const pizzaController = {
   // get all pizzas || callback func for GET/api/pizzas route
   getAllPizza(req, res) {
     Pizza.find({})
+      // populates the mongoDB field with data by passing in an object w/a key path
+      .populate({
+        path: "comments",
+        // this ignores __v otherwise would only return v field
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
       .then((dbPizzaData) => res.json(dbPizzaData))
       .catch((err) => {
         console.log(err);
@@ -18,6 +26,11 @@ const pizzaController = {
   getPizzaById({ params }, res) {
     // instead of accessing the entire req, params is destructured out of it
     Pizza.findOne({ _id: params.id })
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      .select("-__v")
       .then((dbPizzaData) => {
         // If no pizza is found, send 404
         if (!dbPizzaData) {
@@ -42,8 +55,8 @@ const pizzaController = {
 
   // update pizza by id || .updateOne() & .updateMany updates docs without returning them
   updatePizza({ params, body }, res) {
-      // {new: true} returns the updated document
-      // the where clause is used first in mongoose, then data, then return options
+    // {new: true} returns the updated document
+    // the where clause is used first in mongoose, then data, then return options
     Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
       .then((dbPizzaData) => {
         if (!dbPizzaData) {
@@ -56,17 +69,17 @@ const pizzaController = {
   },
 
   // delete pizza || this method provides a little more data unlike .deleteOne() or .deleteMany
-  deletePizza({params}, res) {
-      Pizza.findOneAndDelete({_id: params.id})
-      .then(dbPizzaData => {
-          if (!dbPizzaData) {
-              res.status(404).json({message: 'No pizza found with this id!'});
-              return;
-          }
-          res.json(dbPizzaData);
+  deletePizza({ params }, res) {
+    Pizza.findOneAndDelete({ _id: params.id })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
+        res.json(dbPizzaData);
       })
-      .catch(err => res.status(400).json(err));
-  }
+      .catch((err) => res.status(400).json(err));
+  },
 };
 
 module.exports = pizzaController;
