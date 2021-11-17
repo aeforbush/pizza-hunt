03871fded9e637c6cb4b0c7920c8cb49cbbbd85b
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-11-17 14:09:54 -0700

    added reply routes

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,10 +1,23 @@
-const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const router = require("express").Router();
+const {
+  addComment,
+  removeComment,
+  addReply,
+  removeReply,
+} = require("../../controllers/comment-controller");
 
 // /api/comments/:pizzaId
-router.route('/:pizzaId').post(addComment);
+router.route("/:pizzaId")
+.post(addComment);
 
+// callback function fo a route has req, res as parameters so explicit arugments are unecessary
 // /api/comments/:pizzaId/:commentId
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router.route("/:pizzaId/:commentId")
+.put(addReply)
+.delete(removeComment);
 
-module.exports = router; 
+// using  RESTful so we include the parent resources in the endpoint; kind of like saying "go to this pizza, look at this comment and delete this one reply"
+router.route("/:pizzaId/:commentId/:replyId")
+.delete(removeReply);
+
+module.exports = router;
